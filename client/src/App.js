22eeb8ec17e3c35282bc345555c81e3b77b47cbc
facundo-santacruz <facugs2090@gmail.com>
2021-04-02 11:11:44 -0300

    (client/scss) create and install dependencie scss

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,8 @@
 import './App.css';
 import { BrowserRouter, Route } from "react-router-dom";
 import { Principal } from "./Components/Principal";
+
+import './scss/index.scss';
 function App() {
   return (
     <div className="App">
