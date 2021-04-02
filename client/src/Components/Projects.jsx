ed2c73250343935fd149a16b8e09cef525b0b717
facundo-santacruz <facugs2090@gmail.com>
2021-04-02 17:22:59 -0300

    (client/src/components/Projects)

--- client/src/Components/Projects.jsx
+++ client/src/Components/Projects.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
-
+import '../scss/projects.scss'
 const Projects = () => {
     return (
-        <div>
-            
+        <div className="projects-principal">
+            <h1>Proyectos</h1>
+            <div className="projects-secondary">
+                <span>Meli App</span>
+                <div className="projects-card">
+                    <div className="projects-img">
+                        <img alt="Imagen Mercado Libre" src="https://d1ih8jugeo2m5m.cloudfront.net/2019/11/Ejemplo-4.png"></img>
+                    </div>
+                    <div className="projects-container">
+                        <h5>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Mollitia similique ipsam provident, aliquam esse aliquid quibusdam inventore libero est possimus nulla explicabo ipsum soluta omnis asperiores eius nisi. Omnis, placeat?</h5>
+                        <p>Link: <a href="https://meli-app-facundo-santacruz.herokuapp.com/">¡Click Aquí!</a></p>
+                    </div>
+                </div>
+
+            </div>
         </div>
     )
 }
