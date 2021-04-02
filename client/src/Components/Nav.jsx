732b3d7e2c30b4221ba6a21093c8b96ff5de5e9d
facundo-santacruz <facugs2090@gmail.com>
2021-04-02 13:42:02 -0300

    (client/Components/Nav) add buttons with styles

--- client/src/Components/Nav.jsx
+++ client/src/Components/Nav.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import "../scss/nav.scss"
 
 const Nav = () => {
     return (
-        <div>
-            
+        <div className="principal">
+            <div className="secondary">
+                <Link className="link" to="menu/projects">Proyectos</Link>
+                <Link className="link" to="menu/tecnologies">Tecnologías</Link>
+                <Link className="link" to="menu/about">Sobre mí</Link>  
+            </div>
         </div>
     )
 }
