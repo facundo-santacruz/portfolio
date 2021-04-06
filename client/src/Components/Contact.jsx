f12a91554ddfebb8cef020edd6f73369240c75dd
facundo-santacruz <facugs2090@gmail.com>
2021-04-06 02:34:46 -0300

    (client/src) giving some styles

--- client/src/Components/Contact.jsx
+++ client/src/Components/Contact.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import '../scss/contact.scss'
-
+import { AiOutlineMail } from "react-icons/ai";
 const Contact = () => {
     const [input, setInput] = useState({
         email: "",
@@ -12,7 +12,6 @@ const Contact = () => {
         email: "Formato inválido",
         asunto: "Debe contener más de 2 caracteres.",
         text: "Debe contener más de 2 caracteres."
-        
     }) 
 
     const validation = (text) => {
@@ -53,18 +52,18 @@ const Contact = () => {
     return (
         <form onSubmit={handleSubmit} className="contact-principal">
             <h1>Contacto</h1>
-            <div>
+            <div className="contact-secondary">
                 <div className="contact-third">
-                    <label>Correo</label>
-                    <input type="email" onChange={handleChange} value={input.email} name="email"/>
+                    <label><AiOutlineMail></AiOutlineMail> Correo</label>
+                    <input type="email" placeholder="Ingrese correo." onChange={handleChange} value={input.email} name="email"/>
                 </div>
                 <div className="contact-third">
                     <label>Asunto</label>
-                    <input type="text" onChange={handleChange} value={input.asunto} name="asunto"/>
+                    <input type="text" placeholder="Ingrese asunto." onChange={handleChange} value={input.asunto} name="asunto"/>
                 </div>
                 <div className="contact-third">
                     <label>Mensaje</label>
-                    <textarea rows="5" type="text" onChange={handleChange} value={input.text} name="text"/>
+                    <textarea rows="5" placeholder="Ingrese asunto." type="text" onChange={handleChange} value={input.text} name="text"/>
                 </div>
                 <button disabled={!error} onClick={handleSubmit}>Enviar</button>
             </div>
