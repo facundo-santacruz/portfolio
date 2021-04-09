4367b79fdd0a1ea32fa2ab4c88b634241ecc122b
facundo-santacruz <facugs2090@gmail.com>
2021-04-09 02:29:35 -0300

    (api) creating app.js

--- api/app.js
+++ api/app.js
@@ -1,11 +1,19 @@
 var express = require('express');
 var app = express();
-var cors = require('cors')
-app.use(cors())
+var cors = require('cors');
+const morgan = require('morgan')
+const bodyParser = require('body-parser')
 
+app.use(morgan('tiny'))
+app.use(express.urlencoded({
+  extended: true
+}));
+
+app.use(express.json());
 app.get('/', function (req, res) {
-    console.log(req)
-  res.send('Hola mundo');
+  console.log(req.query)
+  console.log(req.params)
+  res.send(req.query).status(200);
 });
  
 app.listen(4000, function () {
