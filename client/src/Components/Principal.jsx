95f9663b51f0c939f7a41e87dd7dab7a878ac957
facundo-santacruz <facugs2090@gmail.com>
2021-04-13 04:57:36 -0300

    (client/principal)

--- client/src/Components/Principal.jsx
+++ client/src/Components/Principal.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import "../scss/principal.scss";
-
+import imagen from "../imagen/FOTO.jpeg"
 const Principal = () => {
     return (
-        <div>
-            
+        <div className="container-principal">
+        
+            <img className="imagen-container" src={imagen} alt="imagen"/>
         </div>
     )
 }
