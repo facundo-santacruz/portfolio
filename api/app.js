f8da7e806258587f1880489eaf95c42e745f3d42
facundo-santacruz <facugs2090@gmail.com>
2021-04-14 04:04:36 -0300

    (api) download cv

--- api/app.js
+++ api/app.js
@@ -13,11 +13,20 @@ app.use(express.urlencoded({
 }));
 
 app.use(express.json());
+
+
 app.get('/sendMsg', async (req, res) => {
   console.log(req.query)
   const state = await sendEmail(req.query) 
   res.send(state).status(200);
 });
+
+app.get('/download', (req, res) => {
+  res.download(`${__dirname.slice(0,-3)}/client/public/CV Facundo Santacruz.pdf`, (err) => {
+    err ? res.send(err).status(404) : res.send("Archivo descargado").status(200))
+  } )
+
+})
  
 app.listen(app.get('port'), function () {
   console.log(`Example app listening on port ${app.get('port')}!`);
